541754f35f774c0b2b71d3d4b16a41430b75bd78
laCACHITO <ninucat92@gmail.com>
2017-11-06 09:28:30 +0100

    3/11/2017 10.30

--- app/src/main/java/com/example/adri/projectegithub/Hotel.java
+++ app/src/main/java/com/example/adri/projectegithub/Hotel.java
@@ -3,10 +3,12 @@ package com.example.adri.projectegithub;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Hotel extends AppCompatActivity {
+public class Hotel extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hotel);
     }
--- app/src/main/java/com/example/adri/projectegithub/MainActivity.java
+++ app/src/main/java/com/example/adri/projectegithub/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.adri.projectegithub;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void hotels (View v)
+    {
+        startActivity(new Intent(this,Hotel.class));
     }
 }
