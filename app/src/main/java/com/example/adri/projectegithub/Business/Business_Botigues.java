2354501e9cb8ca41ae5cfc43ca9ec44ddc4b41a7
Adri <adriagaca@gmail.com>
2017-11-06 09:02:36 +0100

    06/11/2017

--- app/src/main/java/com/example/adri/projectegithub/Business/Business_Botigues.java
+++ app/src/main/java/com/example/adri/projectegithub/Business/Business_Botigues.java
@@ -1,15 +1,66 @@
 package com.example.adri.projectegithub.Business;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.adri.projectegithub.R;
 
-public class Business_Botigues extends AppCompatActivity {
+import org.w3c.dom.Text;
 
+public class Business_Botigues extends Activity implements View.OnClickListener {
+    Button Back = (Button)findViewById(R.id.Back);
+    TextView Link1 = (TextView)findViewById(R.id.LinkPrimeraBotiga);
+    TextView Link2 = (TextView)findViewById(R.id.LinkSegonaBotiga);
+    TextView Link3 = (TextView)findViewById(R.id.LinkTerceraBotiga);
+    TextView Link4 = (TextView)findViewById(R.id.LinkQuartaBotiga);
+    TextView Link5 = (TextView)findViewById(R.id.LinkCinquenaBotiga);
+    TextView Tlf1 = (TextView)findViewById(R.id.TlfBotiga1);
+    TextView Tlf2 = (TextView)findViewById(R.id.TlfBotiga2);
+    TextView Tlf3 = (TextView)findViewById(R.id.TlfBotiga3);
+    TextView Tlf4 = (TextView)findViewById(R.id.TlfBotiga4);
+    TextView Tlf5 = (TextView)findViewById(R.id.TlfBotiga5);
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_business__botigues);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view==Link1)
+        {
+            Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse("https//:www.google.es"));
+            startActivity(intent);
+        }
+        else if (view==Link2)
+        {
+            Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse("https//:www.google.es"));
+            startActivity(intent);
+        }
+        else if (view==Link3)
+        {
+            Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse("https//:www.google.es"));
+            startActivity(intent);
+        }
+        else if (view==Link4)
+        {
+            Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse("https//:www.google.es"));
+            startActivity(intent);
+        }
+        else if (view==Link5)
+        {
+            Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse("https//:www.google.es"));
+            startActivity(intent);
+        }
+
     }
 }
