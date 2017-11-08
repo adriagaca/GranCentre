25c2aa259a4f32c5e778e87d43d2929006f3370b
Usuario <ninucat92@gmail.com>
2017-11-08 17:52:20 +0100

    SergiTorre

--- app/src/main/java/com/example/adri/projectegithub/MainActivity.java
+++ app/src/main/java/com/example/adri/projectegithub/MainActivity.java
@@ -6,18 +6,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    public ImageView hotel,restaurants;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        hotel = (ImageView) findViewById(R.id.imageView6);
+        hotel.setOnClickListener(this);
+        restaurants = (ImageView) findViewById(R.id.imageView1);
+        restaurants.setOnClickListener(this);
     }
-    public  void gotoresta(View v){
-        startActivity(new Intent(this,Activity_Restaurant.class));
-    }
-    public void hotels (View v)
+
+    @Override
+    public void onClick(View v)
     {
-        startActivity(new Intent(this,Hotel.class));
+        switch (v.getId())
+        {
+            case R.id.imageView6: startActivity(new Intent(this,Hotel.class));
+                break;
+            case R.id.imageView1: startActivity(new Intent(this,Activity_Restaurant.class));
+                break;
+        }
+
     }
 }
