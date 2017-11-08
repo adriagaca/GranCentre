d645ce96ab2aa8f31977c253ffc393daef0d9130
laCACHITO <ninucat92@gmail.com>
2017-11-08 12:55:58 +0100

    3/11/2017 10.30

--- app/src/main/java/com/example/adri/projectegithub/Hotel.java
+++ app/src/main/java/com/example/adri/projectegithub/Hotel.java
@@ -2,9 +2,13 @@ package com.example.adri.projectegithub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class Hotel extends AppCompatActivity
 {
+    private RatingBar ratingBar;
+    private TextView txtRatingValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
