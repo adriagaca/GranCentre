48cbec05948b84c760c0fe7f1467405b2b6588bd
laCACHITO <ninucat92@gmail.com>
2017-11-10 09:57:39 +0100

    menuarreglat

--- app/src/main/java/com/example/adri/projectegithub/MainActivity.java
+++ app/src/main/java/com/example/adri/projectegithub/MainActivity.java
@@ -9,25 +9,32 @@ import android.widget.ImageView;
 import com.example.adri.projectegithub.Business.Business;
 import com.example.adri.projectegithub.Business.Business_Perruqueries;
 
-public class MainActivity extends AppCompatActivity {
+    public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-    public  void gotoresta(View v){
-        startActivity(new Intent(this,Activity_Restaurant.class));
-    }
-    public void hotels (View v)
-    {
-        startActivity(new Intent(this,Business_Perruqueries.class));
-    }
+        public ImageView hotel,restaurants;
 
-    public  void gotobusiness(View v){
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
-        Intent hola = new Intent(this,Business.class);
-        startActivity(hola);
+            hotel = (ImageView) findViewById(R.id.imageView6);
+            hotel.setOnClickListener(this);
+            restaurants = (ImageView) findViewById(R.id.imageView1);
+            restaurants.setOnClickListener(this);
+        }
 
+
+        @Override
+        public void onClick(View v)
+        {
+            switch (v.getId())
+            {
+                case R.id.imageView6: startActivity(new Intent(this,Hotel.class));
+                    break;
+                case R.id.imageView1: startActivity(new Intent(this,Activity_Restaurant.class));
+                    break;
+            }
+
+        }
     }
-}
