4294ab1f226a9700d2e07a0729caae3082eaab6c
Adri <adriagaca@gmail.com>
2017-11-20 13:20:22 +0100

    Projecte final

--- app/src/main/java/com/example/adri/projectegithub/MainActivity.java
+++ app/src/main/java/com/example/adri/projectegithub/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.adri.projectegithub;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,20 +11,32 @@ import android.widget.ImageView;
 import com.example.adri.projectegithub.Business.Business;
 import com.example.adri.projectegithub.Business.Business_Perruqueries;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
+    ImageView restaurant;
+    ImageView business;
+    ImageView hotels;
+    ImageView movies;
+    ImageView weather;
+    ImageView events;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        restaurant =findViewById(R.id.imageView1);
+        restaurant.setOnClickListener(this);
+        business =findViewById(R.id.imageView2);
+        business.setOnClickListener(this);
+        movies =findViewById(R.id.imageView3);
+        movies.setOnClickListener(this);
+        weather =findViewById(R.id.imageView4);
+        weather.setOnClickListener(this);
+        events =findViewById(R.id.imageView5);
+        events.setOnClickListener(this);
+        hotels =findViewById(R.id.imageView6);
+        hotels.setOnClickListener(this);
     }
-    public  void gotoresta(View v){
-        startActivity(new Intent(this,Activity_Restaurant.class));
-    }
-    public void hotels (View v)
-    {
-        startActivity(new Intent(this,Business_Perruqueries.class));
-    }
+
 
     public  void gotobusiness(View v){
 
@@ -32,8 +46,43 @@ public class MainActivity extends AppCompatActivity {
     }
     public  void gotomovies(View v){
 
-        Intent hola = new Intent(this,Movies.class);
-        startActivity(hola);
+        Intent intent = new Intent(this,Movies.class);
+        startActivity(intent);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==restaurant)
+        {
+            Intent intent = new Intent(this,Restaurant.class);
+            startActivity(intent);
+        }
+        else if (v==business)
+        {
+            Intent intent = new Intent(this,Business.class);
+            startActivity(intent);
+        }
+        else if (v==movies)
+        {
+            Intent intent = new Intent(this,Movies.class);
+            startActivity(intent);
+        }
+        else if (v==weather)
+        {
+            Intent intent = new Intent(this,Weather.class);
+            startActivity(intent);
+        }
+        else if (v==events)
+        {
+            Intent intent = new Intent(this,Events.class);
+            startActivity(intent);
+        }
+        else if (v==hotels)
+        {
+            Intent intent = new Intent(this,Hotel.class);
+            startActivity(intent);
+        }
 
     }
 }
